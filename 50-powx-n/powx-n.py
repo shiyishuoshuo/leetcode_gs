5ac8c216439fe193d949c8627d0f39c11a05cc51
Shuo Zhang <shuozhang2@gmail.com>
2024-08-26 22:12:41 -0400

    Time: 30 ms (87.46%) | Memory: 16.6 MB (15.31%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,8 +3,8 @@ class Solution:
         if n < 0:
             return 1.0 / self.myPow(x, -n)
 
-        if n == -2**31:
-            return self.myPow(1 / x, -(n + 1)) / x
+        # if n == -2**31:
+        #     return self.myPow(1 / x, -(n + 1)) / x
         
         if n == 0:
             return 1

84711609ee10886f4f59a211d9e9f118c6279758
Shuo Zhang <shuozhang2@gmail.com>
2024-09-05 21:42:49 -0400

    Time: 31 ms (79.23%) | Memory: 16.6 MB (58.68%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s)
+        first_non_empty_index = n - 1
+        res = 0
+
+        while first_non_empty_index > -1:
+            if s[first_non_empty_index] != ' ':
+                break
+            first_non_empty_index -= 1
+
+        print(f'{first_non_empty_index}')
+        while first_non_empty_index > -1:
+            if s[first_non_empty_index] == ' ':
+                break
+            first_non_empty_index -= 1
+            res += 1
+        
+        return res
+        

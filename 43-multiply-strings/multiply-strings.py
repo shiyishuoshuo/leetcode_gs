1067807a3c7d27d33e61cf491196afdb2852d45f
Shuo Zhang <shuozhang2@gmail.com>
2024-09-08 12:00:54 -0400

    Time: 108 ms (16.75%) | Memory: 16.5 MB (50.27%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -3,7 +3,7 @@ class Solution:
         n1, n2 = len(num1), len(num2)
         res = [0] * (n1 + n2)
 
-        for i in range(n1 - 1, -1, -1):
+        for i in range(n1 -1, -1, -1):
             for j in range(n2 -1, -1, -1):
                 res[i + j + 1] += int(num1[i]) * int(num2[j])
                 carry, res[i + j + 1] = divmod(res[i + j + 1], 10)
@@ -12,9 +12,5 @@ class Solution:
         i = 0
         while i < len(res) and res[i] == 0:
             i += 1
-        
-        str_res = ''.join(str(x) for x in res[i:])
 
-        return '0' if not str_res else str_res
-
-        
+        return '0' if i == len(res) else ''.join([str(n) for n in res[i::]])

c7407bd382bcacdea8e5a0b8f1e5dec9f8c449d8
Shuo Zhang <shuozhang2@gmail.com>
2024-09-07 21:26:00 -0400

    Time: 178 ms (9.99%) | Memory: 17.7 MB (87.96%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -3,7 +3,6 @@ class Solution:
         nums = sorted(nums)
         prev_one, prev_two = nums[0], nums[1]
         res = 0
-        print(f'nums: {nums}')
         for i in range(2, len(nums)):
             cur = nums[i]
             if prev_one + prev_two > cur:

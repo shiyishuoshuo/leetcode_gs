aa0786b8e6302a3754cdbf2643c5e003a7b64cd8
Shuo Zhang <shuozhang2@gmail.com>
2024-09-30 12:34:40 -0400

    Time: 34 ms (88.02%) | Memory: 16.7 MB (12.36%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,30 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        copy_nums1 = nums1[0:m]
-        i, j = 0, 0
-        p = 0
+        p1 = m - 1
+        p2 = n - 1
 
-        while i in range(m) and j in range(n):
-            if copy_nums1[i] <= nums2[j]:
-                nums1[p] = copy_nums1[i]
-                p += 1
-                i +=1
+        for p in range(m + n - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[p] = nums2[j]
-                p += 1
-                j +=1
-        
-        if i in range(m):
-            while i in range(m):
-                nums1[p] = copy_nums1[i]
-                i += 1
-                p +=1
-            
-
-        if j in range(n):
-            while j in range(n):
-                nums1[p] = nums2[j]
-                j += 1
-                p +=1
-        
+                nums1[p] = nums2[p2]
+                p2 -= 1

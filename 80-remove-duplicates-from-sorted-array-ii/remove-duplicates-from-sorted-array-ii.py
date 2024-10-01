1e19eeee08bc3a0a2ed838721d1b26db94441939
Shuo Zhang <shuozhang2@gmail.com>
2024-09-30 20:34:02 -0400

    Time: 46 ms (86.09%) | Memory: 16.6 MB (52.78%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -13,9 +13,6 @@ class Solution:
                 nums[slow] = nums[fast]
             fast += 1
             count += 1
-            # if fast < len(nums) and nums[fast] != nums[fast - 1]:
-            #     # fast 遇到新的不同的元素时，重置 count
-            #     count = 0
         
         return slow + 1
         

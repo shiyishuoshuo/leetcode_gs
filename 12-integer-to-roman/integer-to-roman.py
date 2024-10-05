6fed65d3fe7f2ad4a1b892165c2473a672aa0afd
Shuo Zhang <shuozhang2@gmail.com>
2024-10-05 09:24:30 -0400

    Time: 61 ms (5.76%) | Memory: 16.4 MB (92.71%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,30 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        remainder = num
+        sym_val_map = {
+            1000 : 'M',
+            900 : 'CM',
+            500 : 'D',
+            400 : 'CD',
+            100 : 'C',
+            90 : 'XC',
+            50 : 'L',
+            40 : 'XL',
+            10 : 'X',
+            9 : 'IX',
+            5 : 'V',
+            4 : 'IV',
+            1 : 'I'
+        }
+        res = []
+
+        for val, sym in sym_val_map.items():
+            if num == 0:
+                break
+            print(f'{val}')
+            count, num = divmod(num, val)
+            res.append(sym * count)
+        return ''.join(res)
+
+
+        

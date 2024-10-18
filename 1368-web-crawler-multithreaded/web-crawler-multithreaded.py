4087e52d9a1dbffbe5d5b551d53feb34d0d8b278
Shuo Zhang <shuozhang2@gmail.com>
2024-10-18 07:47:59 -0400

    Time: 219 ms (71.47%) | Memory: 24.5 MB (61.23%) - LeetSync

--- 1368-web-crawler-multithreaded/web-crawler-multithreaded.py
+++ 1368-web-crawler-multithreaded/web-crawler-multithreaded.py
@@ -8,23 +8,25 @@
 #        :type url: str
 #        :rtype List[str]
 #        """
-
 from concurrent import futures
-
 class Solution:
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
         hostname = lambda url: url.split('/')[2]
-        seen = set()
-        seen.add(startUrl)
+        seen = set([startUrl])
+        future_list = set()
 
         with futures.ThreadPoolExecutor(max_workers=16) as executor:
-            tasks = deque([executor.submit(htmlParser.getUrls, startUrl)])
-            while tasks:
-                for url in tasks.popleft().result():
-                    if url not in seen and hostname(startUrl) == hostname(url):
-                        seen.add(url)
-                        tasks.append(executor.submit(htmlParser.getUrls, url))
-        
+            future_list.add(executor.submit(htmlParser.getUrls, startUrl))
+            while future_list:
+                for future in futures.as_completed(future_list):
+                    for url in future.result():
+                        if url not in seen and hostname(url) == hostname(startUrl):
+                            seen.add(url)
+                            future_list.add(executor.submit(htmlParser.getUrls, url))
+                    future_list.remove(future)
+
         return list(seen)
+
+
 
         

cde50d11e1e35fad710aa36f5aeae4b840eb4196
Shuo Zhang <shuozhang2@gmail.com>
2024-10-17 20:45:59 -0400

    Time: 210 ms (83.47%) | Memory: 24.3 MB (80.29%) - LeetSync

---
+++ 1368-web-crawler-multithreaded/web-crawler-multithreaded.py
@@ -0,0 +1,30 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+from concurrent import futures
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        hostname = lambda url: url.split('/')[2]
+        seen = set()
+        seen.add(startUrl)
+
+        with futures.ThreadPoolExecutor(max_workers=16) as executor:
+            tasks = deque([executor.submit(htmlParser.getUrls, startUrl)])
+            while tasks:
+                for url in tasks.popleft().result():
+                    if url not in seen and hostname(startUrl) == hostname(url):
+                        seen.add(url)
+                        tasks.append(executor.submit(htmlParser.getUrls, url))
+        
+        return list(seen)
+
+        

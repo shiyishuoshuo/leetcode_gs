45f1a579bda25271ae498d196d59d4480ca073b6
Shuo Zhang <shuozhang2@gmail.com>
2024-10-20 08:39:37 -0400

    Time: 52 ms (11.35%) | Memory: 17.2 MB (32.47%) - LeetSync

--- 281-zigzag-iterator/zigzag-iterator.py
+++ 281-zigzag-iterator/zigzag-iterator.py
@@ -1,29 +1,34 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
         self.vectors = [v1, v2]
-        self.p_elem = 0   # pointer to the index of element
-        self.p_vec = 0    # pointer to the vector
-        # variables for hasNext() function
-        self.total_num = len(v1) + len(v2)
+        self.index = 0
+        self.current = 0
+        self.size = 2
+        self.total_number = len(v1) + len(v2)
         self.output_count = 0
+        
 
     def next(self) -> int:
-        # Attempt to get the next element from the current vector
-        curr_vec = self.vectors[self.p_vec]
-        if self.p_elem < len(curr_vec):
-            ret = curr_vec[self.p_elem]
+        vector = self.vectors[self.current]
+        if self.index < len(vector):
+            res = vector[self.index]
             self.output_count += 1
-            # Prepare pointers for next call
-            self.p_vec = (self.p_vec + 1) % len(self.vectors)
-            if self.p_vec == 0:
-                self.p_elem += 1
-            return ret
+            self.current = (self.current + 1) % self.size
+            if self.current == 0:
+                self.index += 1
+            return res
         else:
-            # Move to the next vector and retry
-            self.p_vec = (self.p_vec + 1) % len(self.vectors)
-            if self.p_vec == 0:
-                self.p_elem += 1
-            return self.next()  # Recursive call to handle skipping empty indices
+            self.current = (self.current + 1) % self.size
+            if self.current == 0:
+                self.index += 1
+            return self.next()
+        
+        
 
     def hasNext(self) -> bool:
-        return self.output_count < self.total_num
+        return self.output_count < self.total_number
+        
+
+# Your ZigzagIterator object will be instantiated and called as such:
+# i, v = ZigzagIterator(v1, v2), []
+# while i.hasNext(): v.append(i.next())

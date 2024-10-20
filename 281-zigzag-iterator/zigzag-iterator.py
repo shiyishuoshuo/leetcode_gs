91c1652f6016addcef12919e96ff598373e86e72
Shuo Zhang <shuozhang2@gmail.com>
2024-10-19 21:51:40 -0400

    Time: 43 ms (71.41%) | Memory: 17.1 MB (32.47%) - LeetSync

--- 281-zigzag-iterator/zigzag-iterator.py
+++ 281-zigzag-iterator/zigzag-iterator.py
@@ -1,27 +1,29 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
         self.vectors = [v1, v2]
-        self.queue = deque()
-        for index, vector in enumerate(self.vectors):
-            if len(vector) > 0:
-                self.queue.append((index, 0))
-
-        
+        self.p_elem = 0   # pointer to the index of element
+        self.p_vec = 0    # pointer to the vector
+        # variables for hasNext() function
+        self.total_num = len(v1) + len(v2)
+        self.output_count = 0
 
     def next(self) -> int:
-
-        if self.queue:
-            vec_index, elm_index = self.queue.popleft()
-            next_index = elm_index + 1
-            if next_index < len(self.vectors[vec_index]):
-                self.queue.append((vec_index, next_index))
-            return self.vectors[vec_index][elm_index]
-        
+        # Attempt to get the next element from the current vector
+        curr_vec = self.vectors[self.p_vec]
+        if self.p_elem < len(curr_vec):
+            ret = curr_vec[self.p_elem]
+            self.output_count += 1
+            # Prepare pointers for next call
+            self.p_vec = (self.p_vec + 1) % len(self.vectors)
+            if self.p_vec == 0:
+                self.p_elem += 1
+            return ret
+        else:
+            # Move to the next vector and retry
+            self.p_vec = (self.p_vec + 1) % len(self.vectors)
+            if self.p_vec == 0:
+                self.p_elem += 1
+            return self.next()  # Recursive call to handle skipping empty indices
 
     def hasNext(self) -> bool:
-        return len(self.queue) > 0
-        
-
-# Your ZigzagIterator object will be instantiated and called as such:
-# i, v = ZigzagIterator(v1, v2), []
-# while i.hasNext(): v.append(i.next())
+        return self.output_count < self.total_num

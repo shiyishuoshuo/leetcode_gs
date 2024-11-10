81d216f44ec35b7b400e9b2abea4b4e11ef2f0c0
Shuo Zhang <shuozhang2@gmail.com>
2024-11-10 10:19:09 -0500

    Time: 15 ms (59.12%) | Memory: 16.8 MB (81.41%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -13,10 +13,10 @@ class Solution:
             while left < right and unique_count == len(windows):
                 res += len(nums) - right + 1
                 c = nums[left]
-                windows[c] -= 1
-                if windows[c] == 0:
-                    windows.pop(c)
+                if c in windows:
+                    windows[c] -= 1
+                    if windows[c] == 0:
+                        windows.pop(c)
                 left += 1
-            
         
         return res

cb9161037996ebba05b8a4c9e86fb642418c0cb6
Shuo Zhang <shuozhang2@gmail.com>
2024-11-13 08:08:08 -0500

    Time: 329 ms (37.12%) | Memory: 25.3 MB (94.71%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,47 @@
+class MonotonicQueue:
+        def __init__(self):
+            self.max_q = deque([])
+            self.min_q = deque([])
+        
+        def push(self, num:int) -> None:
+            while self.max_q and num > self.max_q[-1]:
+                self.max_q.pop()
+            self.max_q.append(num)
+
+            while self.min_q and num < self.min_q[-1]:
+                self.min_q.pop()
+            self.min_q.append(num)
+        
+        def max(self) -> int:
+            return self.max_q[0]
+
+        def min(self) -> int:
+            return self.min_q[0]
+        
+        def pop(self, num:int) -> None:
+            if self.max_q and num == self.max_q[0]:
+                self.max_q.popleft()
+            if self.min_q and num == self.min_q[0]:
+                self.min_q.popleft()
+class Solution:
+
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        num_len, res = len(nums), -1
+        left, right = 0, 0
+        window = MonotonicQueue()
+        window_size = 0
+
+        while right < num_len:
+            d = nums[right]
+            window.push(d)
+            right += 1
+
+            while left < right and window.max() - window.min() > limit:
+                c = nums[left]
+                window.pop(c)
+                left += 1
+
+            res = max(res, right - left)
+        
+        return res
+        

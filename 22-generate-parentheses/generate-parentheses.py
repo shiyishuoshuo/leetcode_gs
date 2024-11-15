30e66f97dfc4deac7c73daa8d6492dc8e2bd3307
Shuo Zhang <shuozhang2@gmail.com>
2024-11-15 07:21:11 -0500

    Time: 2 ms (39.48%) | Memory: 16.7 MB (86.84%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,7 +2,7 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
 
-        def generateHelper(left:int, right:int, track:List[str], res: List[str]) -> None:
+        def generateHelper(left:int, right:int, track:List[str]) -> None:
             if left > n or right > n:
                 return 
 
@@ -10,18 +10,16 @@ class Solution:
                 res.append("".join(track))
                 return
             
-            if right > left:
-                return
-            
-            track.append('(')
-            generateHelper(left + 1, right, track, res)
-            track.pop()
-
-            track.append(')')
-            generateHelper(left, right + 1, track, res)
-            track.pop()
+            if left < n:
+                track.append('(')
+                generateHelper(left + 1, right, track)
+                track.pop()
+            if left > right:
+                track.append(')')
+                generateHelper(left, right + 1, track)
+                track.pop()
 
 
         res, path = [], []
-        generateHelper(0, 0, path, res)
+        generateHelper(0, 0, path)
         return res

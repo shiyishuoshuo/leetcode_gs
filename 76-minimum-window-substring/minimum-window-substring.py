c7a7919e70b9673751719b28375a9eba112b7660
Shuo Zhang <shuozhang2@gmail.com>
2024-11-24 09:16:09 -0500

    Time: 218 ms (19.41%) | Memory: 27.4 MB (5.46%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -5,24 +5,29 @@ class Solution:
         left, right = 0, 0
         valid = 0
         start, length = 0, float('inf')
+        filtered_s = []
 
-        while right < len(s):
-            d = s[right]
+        for (i, c) in enumerate(s):
+            if c in need:
+                filtered_s.append((i, c))
+
+        while right < len(filtered_s):
+            d = filtered_s[right][1]
             right += 1
-            if d in need:
-                window[d] += 1
-                if window[d] == need[d]:
-                    valid += 1
+            window[d] += 1
+            if window[d] == need[d]:
+                valid += 1
             
             while left < right and valid == len(need):
-                c = s[left]
-                if right - left < length:
-                    start = left
-                    length = right - left
+                c = filtered_s[left][1]
+                print(f'right: {right} and left: {left} and size of filtered_s is {len(filtered_s)}')
+                if filtered_s[right - 1][0] - filtered_s[left][0] < length:
+                    start = filtered_s[left][0]
+                    length = filtered_s[right - 1][0] - filtered_s[left][0]
                 left += 1
-                if c in need:
-                    if window[c] == need[c]:
-                        valid -= 1
-                    window[c] -= 1
+
+                if window[c] == need[c]:
+                    valid -= 1
+                window[c] -= 1
         
-        return "" if length == float('inf') else s[start:start + length]
+        return "" if length == float('inf') else s[start:start + length + 1]

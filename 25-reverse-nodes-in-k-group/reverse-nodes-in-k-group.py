0d98909c5830502a762e2078d2c29d50e67470af
Shuo Zhang <shuozhang2@gmail.com>
2024-12-09 12:29:54 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (12.20%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -5,30 +5,33 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head:
-            return head
-
-        cur, nextGroupHead = head, head
-        for _ in range(k):
-            if not nextGroupHead:
-                return head
-            nextGroupHead = nextGroupHead.next
-
-        last = self.reverseN(cur, k)
-        cur.next = self.reverseKGroup(nextGroupHead, k)
-        return last
-    
-    def reverseN(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # 1 -> 2
         if not head or not head.next:
             return head
-        pre, cur, nxt = None, head, head.next
-        while n > 0:
+        
+        cur, nxt = head, head
+
+        cnt = k
+
+        while cnt > 0 and nxt:
+          nxt = nxt.next
+          cnt -= 1
+        
+        if cnt > 0:
+            return head
+        
+        new_head = self.reverseN(cur, nxt)
+        cur.next = self.reverseKGroup(nxt, k)
+        return new_head
+    
+    def reverseN(self, a: Optional[ListNode], b: Optional[ListNode]):
+        if not a or not a.next:
+            return a
+
+        pre, cur, nxt = None, a, a
+        while cur != b:
+            nxt = cur.next
             cur.next = pre
             pre = cur
-            cur = nxt
-            if nxt.next:
-                nxt = nxt.next
-            n -= 1
-        head.next = cur
+            cur = nxt            
         return pre
+

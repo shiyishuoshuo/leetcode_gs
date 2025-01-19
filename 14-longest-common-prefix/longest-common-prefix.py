b1c40cb4b643b4e76eefd34a68f3cae89733edd4
Shuo Zhang <shuozhang2@gmail.com>
2025-01-19 10:16:12 -0500

    Time: 1 ms (39.03%) | Memory: 17.7 MB (44.53%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,45 +1,13 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.endOfWord = False
-        self.linkedCount = 0
-
-    def addChild(self, char):
-        if char not in self.children:
-            self.children[char] = TrieNode()
-            self.linkedCount += 1
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    
-    def insertWord(self, word):
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                cur.addChild(c)
-            cur = cur.children[c]
-        cur.endOfWord = True
-
-    def searchLongestCommonPrefix(self, word) -> str:
-        node = self.root
-        res = []
-        for char in word:
-            if char in node.children and node.linkedCount == 1 and not node.endOfWord:
-                res.append(char)
-                node = node.children[char]  
-            else:
-                break
-        return ''.join(res)
-
 class Solution:
-
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        trieTree = Trie()
-        if len(strs) == 1:
-            return strs[0]
-        for i in range(1, len(strs)):
-            trieTree.insertWord(strs[i])
-        return trieTree.searchLongestCommonPrefix(strs[0])
-        
+        res = ""
+
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                cur = strs[j]
+                if i == len(cur) or cur[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+
+        return res
         
